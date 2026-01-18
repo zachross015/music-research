054aa3709e760ab2a7faadfeba27ea5d9c6e4721
Zachary Ross <zachross015@gmail.com>
2026-01-18 14:33:48 -0500

    new eek

--- sort.py
+++ sort.py
@@ -5,9 +5,6 @@ import shutil
 from datetime import datetime, timedelta
 from collections import defaultdict
 
-# Number of days at the start of a period to consider as buffer for previous period. Must be between 0 and 6.
-BUFFER_DAYS = 4
-
 INPUT_DIR = '.'
 SCRATCHPAD = 'scratchpad.md'
 ARCHIVE_DIR = 'archive'
@@ -108,21 +105,12 @@ def organize_entries(entries):
     entries_by_start_date = defaultdict(list)
     for dt, body in entries:
         iso = dt.isocalendar()
-        year = dt.year
+        year = iso[0]
         week = iso[1]
-        if iso[2] <= BUFFER_DAYS:
-            print("Buffer period triggered: using previous week.")
-            if week == 1:
-                year -= 1
-                # Last week of previous year
-                last_week = datetime(year, 12, 28).isocalendar()[1]
-                week = last_week
-            else:
-                week -= 1
         start_date = datetime.fromisocalendar(year, week, 1)
         date_str = dt.strftime('%m-%d-%Y')
         header = f"# {date_str}"
-        entries_by_start_date[start_date].append((header, body))
+        entries_by_start_date[start_date].append((dt, header, body))
     return entries_by_start_date
 
 def process_entries(entries):
@@ -138,7 +126,10 @@ def process_entries(entries):
 
         existing = load_week_file(week_file)
 
-        for header, body in day_entries:
+        # Sort entries chronologically by creation time
+        sorted_entries = sorted(day_entries, key=lambda x: x[0])
+        
+        for dt, header, body in sorted_entries:
             if header in existing:
                 existing[header] += f"\n\n{body}"
             else:
